4ca49789d4b5581a31d0ea1d71a99f140f202433
BBDBBD0166 <mibbbd@hotmail.com>
2017-08-22 21:37:25 +0800

    第一次提交

---
+++ Lecture01-Examples/Examples2/MainFrom.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Examples2
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(63, 248);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(280, 61);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "選我選我";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(57, 112);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(157, 31);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "請輸入姓名:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(220, 108);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(237, 38);
+            this.nameTextBox.TabIndex = 2;
+            this.nameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nameTextBox_KeyDown);
+            this.nameTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.nameTextBox_KeyUp);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(63, 172);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(118, 31);
+            this.messageLabel.TabIndex = 3;
+            this.messageLabel.Text = "訊息文字";
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 506);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.startButton);
+            this.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MainFrom";
+            this.Text = "第一個視窗程式";
+            this.Load += new System.EventHandler(this.MainFrom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
---
+++ Lecture01-Examples/Examples2/MainFrom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examples2
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            messageLabel.Text = string.Format("Hello{0}", nameTextBox.Text);
+            //MessageBox.Show("Hello" + nameTextBox.Text)
+        }
+
+        private void MainFrom_Load(object sender, EventArgs e)
+        {
+            messageLabel.Text = "";
+        }
+
+        private void nameTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            messageLabel.Text = string.Format("Hello{0}", nameTextBox.Text);
+            nameTextBox.BackColor = SystemColors.Info;
+        }
+
+        private void nameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            nameTextBox.BackColor = SystemColors.MenuHighlight;
+        }
+    }
+}
